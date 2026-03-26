6133fe63b5148f1a25aa6f081146835a6e299fb6
Raghav N. <raghavna37@gmail.com>
2026-03-26 15:37:29 -0400

    Link for website
    https://v0-vite-react-project-two.vercel.app/

---
+++ b_1jJplWjBqdX-1774553787827 2/vite.config.ts
@@ -0,0 +1,30 @@
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/fraserhacks/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
